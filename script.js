4050e238b863973b475215b82e3ab5b15cf8b121
Gabriel Martins Barraqui <gabrielmartinsbarraqui@gmail.com>
2023-05-24 16:49:07 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const currentOperationText = document.querySelector("#current-operation");
+
+
+function number(num) {
+    let numero = currentOperationText.innerHTML;
+    currentOperationText.innerHTML = numero + num;
+}
+
+function clean() {
+    currentOperationText.innerHTML = ""; 
+}
+
+function back() {
+    let apagar = currentOperationText.innerHTML
+    currentOperationText.innerHTML = apagar.substring(0, apagar.length -1);
+}
+
+function calcular() {
+    let resultado = currentOperationText.innerHTML;
+    if(resultado) {
+        currentOperationText.innerHTML = eval(resultado);
+    } else {
+        currentOperationText.innerHTML = "Resultado indefinido"
+    }
+}
